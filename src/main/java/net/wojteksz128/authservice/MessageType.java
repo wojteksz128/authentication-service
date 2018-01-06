3d98d80918b6c8ee45d0b1d009f82357fa775fd5
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-01-06 17:24:18 +0100

    Improve messages

---
+++ src/main/java/net/wojteksz128/authservice/MessageType.java
@@ -0,0 +1,8 @@
+package net.wojteksz128.authservice;
+
+@SuppressWarnings("unused")
+public enum MessageType {
+    INFO,
+    WARNING,
+    ERROR
+}
--- src/main/java/net/wojteksz128/authservice/webapp/WebAppController.java
+++ src/main/java/net/wojteksz128/authservice/webapp/WebAppController.java
@@ -1,5 +1,6 @@
 package net.wojteksz128.authservice.webapp;
 
+import net.wojteksz128.authservice.MessageType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,10 @@ class WebAppController {
 
     @RequestMapping("/signIn")
     public String greeting(Model model) {
+        model.addAttribute("message_type", MessageType.ERROR);
+        model.addAttribute("message_title", "Błąd!");
+        model.addAttribute("message_content", "Podano nieprawidłowy e-mail lub hasło.");
+        model.addAttribute("message_show", true);
         return "login";
     }
 }
