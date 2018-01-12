349a5cadcf35f46f3f1104d5635d66b63b17e16c
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-01-12 08:45:17 +0100

    Add import to DB

---
+++ src/main/resources/import.sql
@@ -0,0 +1,23 @@
+------------------------------------------------------------------------------------------------------------------------
+--  Role użytkowników systemu
+------------------------------------------------------------------------------------------------------------------------
+INSERT INTO roles(code) VALUES ('DEVELOPER');
+INSERT INTO roles(code) VALUES ('USER');
+
+------------------------------------------------------------------------------------------------------------------------
+--  Użytkownicy systemu
+------------------------------------------------------------------------------------------------------------------------
+
+-- dev@t.t : testowo
+INSERT INTO users(email, password) VALUES ('dev@t.t', '$2a$10$n6oWuD9C.oxM5uRdXJ6SOu5cURrH63F51Mvu3m6xnds.1cIb0Tuc6');
+-- user@t.t : Ucze123
+INSERT INTO users(email, password) VALUES ('user@t.t', '$2a$10$xcXvK6JHUwZvrRydIndjrOBAu57t88iasmnWeCfaqcxU6WOS1Wg8O');
+-- wojteksz128@o2.pl : ^joRemofudo
+INSERT INTO users(email, password) VALUES ('wojteksz128@o2.pl', '$2a$10$e1I46ftnWWReJQWjB9T7mu6YMP11k5dP3FRuMWs7G9SRky.icGM3m');
+
+------------------------------------------------------------------------------------------------------------------------
+--  Przypisanie ról do użytkowników
+------------------------------------------------------------------------------------------------------------------------
+INSERT INTO user_roles ( SELECT u.id, r.id FROM users u, roles r WHERE u.email = 'dev@t.t' );
+INSERT INTO user_roles ( SELECT u.id, r.id FROM users u, roles r WHERE u.email = 'user@t.t' AND r.code = 'USER' );
+INSERT INTO user_roles ( SELECT u.id, r.id FROM users u, roles r WHERE u.email = 'wojteksz128@o2.pl' AND r.code = 'USER' );
