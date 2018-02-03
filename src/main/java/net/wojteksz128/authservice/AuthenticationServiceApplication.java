f7357595b02250d717e35b635506fbf3642a80c7
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-02-03 18:15:26 +0100

    Enable OAuth2 Authentication provider

--- src/main/java/net/wojteksz128/authservice/AuthenticationServiceApplication.java
+++ src/main/java/net/wojteksz128/authservice/AuthenticationServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableAutoConfiguration
+@EnableAuthorizationServer
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
