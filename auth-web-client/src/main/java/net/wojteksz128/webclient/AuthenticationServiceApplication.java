92696e5eedf25512c6877b6cd07dfeba88bf9118
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-02-12 00:40:39 +0100

    Configure authentication using authserver

--- auth-web-client/src/main/java/net/wojteksz128/webclient/AuthenticationServiceApplication.java
+++ auth-web-client/src/main/java/net/wojteksz128/webclient/AuthenticationServiceApplication.java
@@ -1,18 +1,20 @@
 package net.wojteksz128.webclient;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@EnableAutoConfiguration
+@ComponentScans({
+    @ComponentScan("net.wojteksz128.authservice.service"),
+    @ComponentScan("net.wojteksz128.webclient")
+})
+@EntityScan("net.wojteksz128.authservice.service")
 public class AuthenticationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceApplication.class, args);
+    }
 }
--- auth-web-client/src/main/java/net/wojteksz128/webclient/security/WebAppSecurityConfig.java
+++ auth-web-client/src/main/java/net/wojteksz128/webclient/security/WebAppSecurityConfig.java
@@ -1,50 +1,34 @@
 package net.wojteksz128.webclient.security;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableOAuth2Sso
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final UserDetailsService userDetailsService;
-
-    private final PasswordEncoder passwordEncoder;
 
     private final AccessDeniedHandler accessDeniedHandler;
 
-    public WebAppSecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder, AccessDeniedHandler accessDeniedHandler) {
-        this.userDetailsService = userDetailsService;
-        this.passwordEncoder = passwordEncoder;
+    public WebAppSecurityConfig(AccessDeniedHandler accessDeniedHandler) {
         this.accessDeniedHandler = accessDeniedHandler;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-            .and().formLogin()/*.loginPage("/signIn")*/.permitAll()
-            .and().logout().permitAll()
-            .and().exceptionHandling().accessDeniedPage("/403").accessDeniedHandler(accessDeniedHandler);
+        http.antMatcher("/**").authorizeRequests()
+            .antMatchers("/", "/login**").permitAll()
+            .anyRequest().authenticated()
+            /*.and().exceptionHandling().accessDeniedPage("/403").accessDeniedHandler(accessDeniedHandler)*/;
     }
 
     @Override
     public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/**/css/**", "/**/js/**", "/webjars/**");
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .userDetailsService(userDetailsService)
-            .passwordEncoder(passwordEncoder);
     }
 }
