9b686124804deb68cdc28b4189083ec55a5f5d8a
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-04-07 11:31:43 +0200

    Add OAuth2 client details entity

---
+++ services/src/main/java/net/wojteksz128/authservice/service/oauth/impl/StringListConverter.java
@@ -0,0 +1,23 @@
+package net.wojteksz128.authservice.service.oauth.impl;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Converter
+class StringListConverter implements AttributeConverter<List<String>, String> {
+
+    private final String DELIMITER = ",";
+
+    @Override
+    public String convertToDatabaseColumn(List<String> strings) {
+        return String.join(DELIMITER, strings);
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String dbData) {
+        return new ArrayList<>(Arrays.asList(dbData.split(DELIMITER)));
+    }
+}
