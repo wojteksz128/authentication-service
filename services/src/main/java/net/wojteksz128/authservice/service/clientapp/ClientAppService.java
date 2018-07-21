efdf6e3aa38f6955d16ac69ccc57371bc09f0721
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-07-21 12:04:43 +0200

    Create ClientAppService and this empty implementation

---
+++ services/src/main/java/net/wojteksz128/authservice/service/clientapp/ClientAppService.java
@@ -0,0 +1,20 @@
+package net.wojteksz128.authservice.service.clientapp;
+
+import net.wojteksz128.authservice.service.exception.EmptyObjectException;
+import net.wojteksz128.authservice.service.exception.InvalidRequestException;
+import net.wojteksz128.authservice.service.exception.ObjectNotCorrespondingException;
+
+import java.util.List;
+
+public interface ClientAppService {
+
+    ClientAppDto createNew(CreateClientAppDto newApp) throws EmptyObjectException;
+
+    ClientAppDto getAppByClientId(String clientId);
+
+    void updateApp(String clientId, UpdateClientAppDto updatedApp) throws ObjectNotCorrespondingException, InvalidRequestException, EmptyObjectException;
+
+    void deleteApp(String clientId, ClientAppDto app) throws ObjectNotCorrespondingException, InvalidRequestException, EmptyObjectException;
+
+    List<ClientAppDto> getAllUserApps(Long userId);
+}
---
+++ services/src/main/java/net/wojteksz128/authservice/service/clientapp/impl/ClientAppServiceImpl.java
@@ -0,0 +1,49 @@
+package net.wojteksz128.authservice.service.clientapp.impl;
+
+import net.wojteksz128.authservice.service.clientapp.*;
+import net.wojteksz128.authservice.service.exception.EmptyObjectException;
+import net.wojteksz128.authservice.service.exception.InvalidRequestException;
+import net.wojteksz128.authservice.service.exception.ObjectNotCorrespondingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ClientAppServiceImpl implements ClientAppService {
+
+    private final ClientAppController clientAppController;
+
+    @Autowired
+    public ClientAppServiceImpl(ClientAppController clientAppController) {
+        this.clientAppController = clientAppController;
+    }
+
+    @Override
+    public ClientAppDto createNew(CreateClientAppDto newApp) throws EmptyObjectException {
+        if (newApp == null) {
+            throw new EmptyObjectException("Attempt to save a null ClientApp object.");
+        }
+        return null;
+    }
+
+    @Override
+    public ClientAppDto getAppByClientId(String clientId) {
+        return null;
+    }
+
+    @Override
+    public void updateApp(String clientId, UpdateClientAppDto updatedApp) throws ObjectNotCorrespondingException, InvalidRequestException, EmptyObjectException {
+
+    }
+
+    @Override
+    public void deleteApp(String clientId, ClientAppDto app) throws ObjectNotCorrespondingException, InvalidRequestException, EmptyObjectException {
+
+    }
+
+    @Override
+    public List<ClientAppDto> getAllUserApps(Long userId) {
+        return null;
+    }
+}
