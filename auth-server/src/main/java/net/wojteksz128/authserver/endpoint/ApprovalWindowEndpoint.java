fd94453aeec79fa4176718b02b22556e4b255788
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-07-30 21:24:49 +0200

    Change approvals endpoint view

--- auth-server/src/main/java/net/wojteksz128/authserver/endpoint/ApprovalWindowEndpoint.java
+++ auth-server/src/main/java/net/wojteksz128/authserver/endpoint/ApprovalWindowEndpoint.java
@@ -1,19 +1,17 @@
 package net.wojteksz128.authserver.endpoint;
 
+import net.wojteksz128.authservice.service.webapp.WebsiteBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @SessionAttributes("authorizationRequest")
 class ApprovalWindowEndpoint {
 
     @GetMapping("/oauth/confirm_access")
-    public String getAccessConfirmation(Model model, HttpServletRequest request) {
-        model.addAttribute("defaultScopes", request.getAttribute("scopes"));
-        return "confirm_access";
+    public String getAccessConfirmation(Model model) {
+        return WebsiteBuilder.create(model).withContent("confirm_access").build();
     }
 }
