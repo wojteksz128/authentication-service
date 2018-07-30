e7947dead00ba0ee81bdc0de909f62fa9996a2a4
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-07-30 22:07:21 +0200

    Change viewed values

--- auth-server/src/main/java/net/wojteksz128/authserver/endpoint/ApprovalWindowEndpoint.java
+++ auth-server/src/main/java/net/wojteksz128/authserver/endpoint/ApprovalWindowEndpoint.java
@@ -1,17 +1,39 @@
 package net.wojteksz128.authserver.endpoint;
 
+import net.wojteksz128.authservice.service.UserDetailsType;
+import net.wojteksz128.authservice.service.clientapp.ClientAppService;
+import net.wojteksz128.authservice.service.exception.EmptyObjectException;
 import net.wojteksz128.authservice.service.webapp.WebsiteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.provider.AuthorizationRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 @SessionAttributes("authorizationRequest")
 class ApprovalWindowEndpoint {
 
+    private final ClientAppService clientAppService;
+
+    @Autowired
+    public ApprovalWindowEndpoint(ClientAppService clientAppService) {
+        this.clientAppService = clientAppService;
+    }
+
     @GetMapping("/oauth/confirm_access")
-    public String getAccessConfirmation(Model model) {
+    public String getAccessConfirmation(Model model) throws EmptyObjectException {
+        final AuthorizationRequest authorizationRequest = (AuthorizationRequest) model.asMap().get("authorizationRequest");
+
+        model.addAttribute("appName", clientAppService.getAppByClientId(authorizationRequest.getClientId()).getName());
+
+        final List<UserDetailsType> scopes = authorizationRequest.getScope().stream().map(UserDetailsType::valueOf).collect(Collectors.toList());
+        model.addAttribute("scopes", scopes);
+
         return WebsiteBuilder.create(model).withContent("confirm_access").build();
     }
 }
--- services/src/main/java/net/wojteksz128/authservice/service/UserDetailsType.java
+++ services/src/main/java/net/wojteksz128/authservice/service/UserDetailsType.java
@@ -1,8 +1,24 @@
 package net.wojteksz128.authservice.service;
 
 public enum UserDetailsType {
-    FULL_NAME,
-    BIRTH_DATE,
-    E_MAIL,
-    CONTACT_DATA
+    FULL_NAME("Imię i nazwisko użytkownika"),
+    BIRTH_DATE("Data urodzenia"),
+    E_MAIL("Adres e-mail"),
+    CONTACT_DATA("Dane kontaktowe");
+
+    private final String scopeName;
+    private final String displayName;
+
+    UserDetailsType(String displayName) {
+        this.scopeName = "scope." + this.name();
+        this.displayName = displayName;
+    }
+
+    public String getScopeName() {
+        return scopeName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
